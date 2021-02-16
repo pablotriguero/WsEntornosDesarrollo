3f92d1447ea691df2eccd63cb39caf931b15bd9b
Felix de Pablo <f.depablo.lobo@gmail.com>
2021-02-16 11:13:32 +0100

    add getters y setters y constructores de motor

--- 03_Ejercicio2GIT/src/entidad/Motor.java
+++ 03_Ejercicio2GIT/src/entidad/Motor.java
@@ -4,6 +4,34 @@ public class Motor {
 	private int id;
 	private String tipoMotor;
 	private int potencia;
+		
+	public Motor() {
+		super();
+	}
+	public Motor(int id, String tipoMotor, int potencia) {
+		super();
+		this.id = id;
+		this.tipoMotor = tipoMotor;
+		this.potencia = potencia;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipoMotor() {
+		return tipoMotor;
+	}
+	public void setTipoMotor(String tipoMotor) {
+		this.tipoMotor = tipoMotor;
+	}
+	public int getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
 	
 	
 }
