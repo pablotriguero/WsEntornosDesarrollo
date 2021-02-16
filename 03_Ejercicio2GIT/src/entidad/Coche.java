d11aed48832c7effefcc6f8238555d68911c6d8a
Felix de Pablo <f.depablo.lobo@gmail.com>
2021-02-16 10:52:17 +0100

    creado el proyecto 3 y la clase Coche

---
+++ 03_Ejercicio2GIT/src/entidad/Coche.java
@@ -0,0 +1,49 @@
+package entidad;
+
+//Estructura JavaBean o POJO, atributos privados y getters y setters publicos
+//Tambien tiene que tener el constructor por defecto o constructor vacio
+public class Coche {
+	private int id;
+	private String matricula;
+	private String marca;
+	private String modelo;
+		
+	public Coche() {
+		super();
+	}
+	
+	public Coche(int id, String matricula, String marca, String modelo) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+	
+}
