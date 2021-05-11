dd6aa32a8cea53da9d4969e9fb7160ed479f2ca1
Felix de Pablo <f.depablo.lobo@gmail.com>
2021-05-11 11:35:02 +0200

    pruebas unitarias

---
+++ 09_JUnit5/test/junit5/_00_Anotaciones.java
@@ -0,0 +1,97 @@
+package junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Felix
+ *
+ * Esta clase seria un ejemplo de auto generada
+ * (boton derecho -> new -> other -> JUnit Test Case
+ * Aunque se pueden llamar como quieran los metodos, estos serian los 
+ * nombres recomendados
+ *
+ */
+class _00_Anotaciones {
+
+	// Tenemos metodos que se ejecutaran en distinto momentos del ciclo de vida 
+	//de una clase de pruebas
+	
+	// El ciclo de vida de una clase de pruebas, sera la ejecución de todos
+	//los metodos anotados con "@Test". 
+	// Pero tambien tenemos otras anotaciones que se ejecutaran
+	
+	// Si quereis que algun metodo no haga nada, simplemente lo dejais vacio
+	
+	// Este metodo con la anotacion @BeforeAll se va a ejecutar ANTES de TODOS 
+	//los Test (metodos anotados con @Test). Esta anotacion es unica
+	//Ejemplo, se ejecutara antes del test1 y solo esa vez
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		// Aqui suelen inicializarse valores o objetos que vayan a ser 
+		//utilizados por todo nuestros casos de prueba, como puede ser
+		//una conexión a la BBDD
+		
+		System.out.println("Este metodo se ejecuta una unica vez y al principio");
+	}
+
+	// Este método con la anotacion @AfterAll sera ejecutado DESPUES de TODOS 
+	//los test
+	//Ejemplo, se ejecutara despues del test2 y solo esa vez
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		//Cerrar conexiones con BBDD, cerrar ficheros,...
+		System.out.println("Este metodo se ejecuta una unica vez y al final");		
+	}
+
+	// Este método con la anotacion @BeforeEach sera ejecutado ANTES de cada 
+	//UNO de los TEST
+	//Ejemplo, se ejecutara antes de test1 y antes de test2 (2 veces)
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("Este metodo se ejecuta una vez antes de cada test");
+	}
+
+	// Este método con la anotacion @AfterEach sera ejecutado DESPUES de cada 
+	//UNO de los TEST
+	//Ejemplo, se ejecutara despues del test1 y despues del test2
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("Este metodo se ejecuta una vez despues de cada test");
+	}
+
+	//Luego irian todos los test unitarios.
+	
+	//Estos serian los metodos que ejecutaran las pruebas unitarios, 
+	//son metodos con la anotacion @Test, y podemos tener todos los 
+	//metodos que queramos con esta anotacion
+	@Test
+	void test1() {
+		System.out.println("Esto es una prueba unitaria de palo :P");
+	}
+
+	@Test
+	void test2() {
+		System.out.println("·Esto es otro prueba unitaria de palo :)");
+	}
+	
+	//Ejemplo de ejecucion para esta clase
+	//boton derecho sobre la clase -> Run us -> JUnit Test
+	
+	/*
+	 * 1 BeforeAll
+	 * 2 BeforeEach
+	 * 3 test1
+	 * 4 AfterEach
+	 * 5 beforeEach
+	 * 6 test2
+	 * 7 afterEach
+	 * 8 afterAll
+	 */
+	
+}
---
+++ 09_JUnit5/test/junit5/_01_Asserciones.java
@@ -0,0 +1,210 @@
+package junit5;
+
+//OJO con la manera de importar los asserts, son metodos estaticos de la clase
+//Assert y por tanto hay que importarlos de manera estatica (import static)
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import programa.Persona;
+
+class _01_Asserciones {
+
+	/*
+	 * Podemos ejecutarlas con boton derecho -> run as.. -> JUnit Test
+	 * 
+	 * Posibles colores al lanzar la prueba:
+	 * 
+	 * 1) Verde, todas las pruebas han ido bien
+	 * 2) Azul, alguna prueba ha ido mal, alguna aserción no se ha 
+	 * cumplido o se ha ejecutado la función fail()
+	 * 3) Rojo, ha habido algun error en la prueba, digamos que no es 
+	 * concluyente y se debería revisar
+	 * 
+	 * Para dar por pasadas las pruebas debería se salir el color verde
+	 */
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Esto se ejecuta UNA sola vez, al PRINCIPIO de todos los test");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("Esto se ejecuta UNA sola vez, al FINAL de todo");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("Se ejecuta ANTES de cada test");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("Se ejecuta DESPUES de cada test");
+	}
+
+	@Test
+	public void acabaEnMundoTest() {
+		System.out.println("Test 1");
+		//Este ejemplo es como si la clase String de java no se hubiera
+		//probado y la gente de oracle nos encargara que nos asegurarmos
+		//que sus metodos funcionan
+		String cadena = new String("Hola mundo");
+		// Las funciones assert son funciones de aceptación
+		
+		// Sirven para dar por valida una prueba, si no cumple un assert 
+		//la prueba sera fallida
+		// Si lo cumple, la prueba sera valida
+
+		// Un assert es determinante para dar por buena una prueba. En 
+		//cuanto se ejecute un assert y ese assert de FALLO, se acaba la 
+		//prueba
+		
+		//Si el assert no da fallo, seguiria con la ejecución de la prueba 
+		//(pudiendo poner otros asserts)
+		
+		//Normalmente estos metodos trabajan con clases y metodos del 
+		//programa principal para probar su funcionamiento
+		
+		//Este assert comprueba que el resultado sea "true"
+		assertTrue(cadena.endsWith("mundo"));
+		//Este ejemplo sería como si yo estuviera testeando la clase
+		//String
+	}
+	
+	@Test
+	public void noAcabaEnHolaTest() {
+		System.out.println("Test 2");
+		String cadena = new String("Hola mundo");
+		//En este caso esperamos que sea falso
+		assertFalse(cadena.endsWith("hola"));//FASE, dariamos por buena la prueba
+	}
+	
+	@Test
+	public void esIgualAHolaMundoTest() {
+		System.out.println("Test 3");
+		String cadena1 = "Hola mundo";
+		String cadena2 = new String("Hola mundo");
+		//En este caso esperamos que sean iguales 
+		//(según el metodo equals())
+		assertEquals(cadena1,cadena2);
+		//No solo para String, para todos los primitivos, objetos, etc
+	}
+	
+	@Test
+	public void noEsIgualAPepeTest() {
+		System.out.println("Test 4");
+		String cadena = new String("Hola mundo");
+		//Que no sean iguales (según el metodo equals())
+		assertNotEquals("Pepe",cadena);
+	}
+
+	@Test
+	public void esNullTest() {
+		System.out.println("Test 5");
+		String cadena = null;
+		//esperamos que sea nulo
+		assertNull(cadena);
+	}
+	
+	@Test
+	public void noEsNullTest() {
+		System.out.println("Test 6");
+		String cadena = new String("Hola mundo");
+		//Esperamos que no sea nulo
+		assertNotNull(cadena);
+	}
+	//Hasta aquí los más importantes
+	
+	
+	//Podemos tambien que ocurra alguna excepcion
+	@Test
+	public void exceptionTest() {
+		System.out.println("Test 7");
+		
+		String cadena = new String("Hola mundo");
+		
+		//Hay que hacer una funcion lambda
+		Exception exception = assertThrows(
+				IndexOutOfBoundsException.class, 
+				() -> cadena.charAt(20));
+		
+		//la excepcion capturada es IndexOutOfBoundsException
+		//por lo que el mensaje que tendrá será el siguiente
+	}
+	
+	//Tiempo maximo para que se ejecute el test (en ms)
+	@Test
+	@Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+	public void timeTest() throws InterruptedException{
+		System.out.println("Test 8");
+		String cadena = new String("Hola mundo");
+		
+		Thread.sleep(50);
+		//Si se demora más de 100ms no se ejecuta más codigo
+		//se parará la ejecución
+		System.out.println("Fin del Test 8");
+	}
+	
+	@Test
+	public void assertArrayEqualsTest() throws InterruptedException{
+		System.out.println("Test 9");
+		List<String> lista1 = new ArrayList<>();
+		lista1.add("Pepe");
+		lista1.add("Ana");
+		List<String> lista2 = new ArrayList<>();
+		lista2.add("Pepe");
+		lista2.add("Ana");
+		//Con esto comprobamos que sean igual y que tengan 
+		//los mismos elementos 2 arrays
+		assertArrayEquals(lista1.toArray(), lista2.toArray());
+	}
+	
+	@Test
+	public void assertSameTest() throws InterruptedException{
+		System.out.println("Test 10");
+		Persona p1 = new Persona();
+		//s1 = "Hola";
+		Persona p2 = new Persona();
+		
+		p1 = p2;
+		//Comprobamos si apuntan al mismo objeto(no invoca a equal, 
+		//es si la referencia apunta al mismo objeto)
+		assertSame(p1, p2);//este test falla, ya que son objetos diferentes
+		//tambien tenemos assertSame(s1,s2)
+	}
+	
+	/*
+	@Test
+	public void failTest() throws InterruptedException{
+		System.out.println("Test 11");
+		//podemos fallar el test bajo alguna cóndición, si se llega a ejecutar fail() en algun momento
+		//se da por fallada la prueba
+		if(true) {
+			fail("Con esto fallamos");
+		}
+		System.out.println("Fin del Test 9");
+	}*/
+
+}
